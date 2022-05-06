ebfc1fab2132b88da2d36a3df6613b82b4f6cde4
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2022-05-06 08:22:29 +0630

    seed file prepare with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+20.times do
+ 	Article.create(title: Faker::Book.title, body: Faker::Lorem.paragraph_by_chars)
+end
+
